65bf6ab6678eb70a1845e32ea5f4c72dd9815168
MindeurFou <tanguy.pouriel@utt.fr>
2021-06-24 22:30:24 +0200

    update PlayerDaoImpl.kt

--- src/database/player/PlayerDaoImpl.kt
+++ src/database/player/PlayerDaoImpl.kt
@@ -3,11 +3,8 @@ package com.mindeurfou.database.player
 import com.mindeurfou.model.player.Player
 import com.mindeurfou.model.player.PostPlayerBody
 import com.mindeurfou.model.player.PutPlayerBody
-import org.jetbrains.exposed.sql.deleteWhere
-import org.jetbrains.exposed.sql.insert
-import org.jetbrains.exposed.sql.select
+import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.transactions.transaction
-import org.jetbrains.exposed.sql.update
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 
@@ -25,12 +22,12 @@ class PlayerDaoImpl : PlayerDao, KoinComponent {
     }
 
     override fun insertPlayer(postPlayer: PostPlayerBody): Int = transaction {
-        playerTable.insert {
+        playerTable.insertAndGetId {
             it[name] = postPlayer.name
             it[lastName] = postPlayer.lastName
             it[username] = postPlayer.username
             it[drawableResourceId] = postPlayer.drawableResourceId
-        }[playerTable.id].value
+        }.value
     }
 
     override fun updateUser(playerId: Int, putPlayer: PutPlayerBody): Player? {
@@ -43,8 +40,8 @@ class PlayerDaoImpl : PlayerDao, KoinComponent {
         return getPlayerById(playerId)
     }
 
-    override fun deleteUser(playerId: Int): Boolean {
-        return playerTable.deleteWhere { (playerTable.id eq playerId) } > 0
+    override fun deleteUser(playerId: Int) = transaction {
+        playerTable.deleteWhere { (playerTable.id eq playerId) } > 0
     }
 
     override fun getPlayerByUsername(username: String): Player? = transaction {
