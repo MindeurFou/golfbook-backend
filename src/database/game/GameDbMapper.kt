78529979eadc6e6b2a95c821d49b23d3a217ed01
MindeurFou <tanguy.pouriel@utt.fr>
2022-03-19 11:07:46 +0100

    Fix wrong definition of Game

--- src/database/game/GameDbMapper.kt
+++ src/database/game/GameDbMapper.kt
@@ -1,11 +1,12 @@
 package com.mindeurfou.database.game
 
 import com.mindeurfou.model.game.outgoing.Game
+import com.mindeurfou.model.player.outgoing.Player
 import org.jetbrains.exposed.sql.ResultRow
 
 object GameDbMapper {
 
-    fun mapFromEntity(resultRow: ResultRow, players: List<String>, courseName: String) = Game(
+    fun mapFromEntity(resultRow: ResultRow, players: List<Player>, courseName: String) = Game(
         id = resultRow[GameTable.id].value,
         state = resultRow[GameTable.state],
         name = resultRow[GameTable.name],
--- src/model/game/outgoing/Game.kt
+++ src/model/game/outgoing/Game.kt
@@ -1,6 +1,7 @@
 package com.mindeurfou.model.game.outgoing
 
 import com.mindeurfou.model.GBState
+import com.mindeurfou.model.player.outgoing.Player
 import com.mindeurfou.utils.DateAsLongSerializer
 import kotlinx.serialization.Serializable
 import java.time.LocalDate
@@ -11,7 +12,7 @@ data class Game(
     val name: String,
     val state: GBState,
     val scoringSystem: ScoringSystem,
-    val players: List<String>,
+    val players: List<Player>,
     val courseName: String,
     @Serializable(with = DateAsLongSerializer::class)
     val createdAt: LocalDate
